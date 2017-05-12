9192f7a8bb9bff5c4b4668af4b8f9f4a62c73080
Andy Robb <andy@andyrobb.com>
2017-05-12 11:49:43 -0700

    Add readme validation instruction to make-dist script

--- make-dist.sh
+++ make-dist.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Use rst2html-2.7.py README.rst /dev/null to validate the formatting of README
+
 # Working directory must be clean for this to create a distribution file cleanly
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 rm -rf $DIR/processrunner.egg-info
