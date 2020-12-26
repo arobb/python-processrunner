e06f7b327e270a67eb061ea6d0ee2694f0734d79
Andy Robb <andy@andyrobb.com>
2020-12-26 16:22:24 -0500

    setup.py: Include Python 3.7 metadata and package structure

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
 
         # Other stuff
         'Environment :: Console',
@@ -65,11 +67,12 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    #, packages=find_packages("processrunner")
+    # , packages=find_packages("processrunner")
+    , packages=["processrunner"]
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    , py_modules=["processrunner"]
+    # , py_modules=["processrunner"]
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
