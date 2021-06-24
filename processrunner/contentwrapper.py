661417c18eef1605ee284145d94d70cb1a8a6903
Andy Robb <andy@andyrobb.com>
2021-06-23 20:03:42 -0400

    contentwrapper.py: Add logging and timing to file-based writes

--- processrunner/contentwrapper.py
+++ processrunner/contentwrapper.py
@@ -3,6 +3,7 @@
 Representation of content for a queue where the values may exceed the native
 pipe size.
 """
+import logging
 import os
 import sys
 import tempfile
@@ -12,6 +13,7 @@ from codecs import getreader
 from .enum import enum
 from .kitchenpatch import getwriter
 from kitchen.text.converters import to_bytes
+from .timer import Timer
 
 # Py2 uses "file" as the base class for IO
 # Used for an isinstance comparison
@@ -42,11 +44,15 @@ class ContentWrapper(object):
 
     def __getattr__(self, attr):
         """When necessary pull the 'value' from a buffer file"""
+        log = object.__getattribute__(self, "_log")
+
         if attr == "value" \
                 and object.__getattribute__(self, "type") == \
                 ContentWrapper.TYPES.FILE:
+            log.debug("Pulling value from buffer file")
             return self._getValueFromFile()
         else:
+            log.debug("Pulling value from memory")
             return object.__getattribute__(self, attr)
 
     def __setattr__(self, attr, val):
@@ -54,6 +60,7 @@ class ContentWrapper(object):
         if attr == "value":
             # Within the threshold size limit
             if len(to_bytes(val)) < ContentWrapper.THRESHOLD:
+                self._log.info("Storing value to memory")
                 object.__setattr__(self, attr, val)
 
             # Larger than what a queue value can hold
@@ -65,8 +72,16 @@ class ContentWrapper(object):
                 handle = object.__getattribute__(self, "locationHandle")
                 object.__setattr__(self, "locationName", handle.name)
                 writer = getwriter("utf-8")(handle)
+
+                self._log.info("Writing value into buffer file {}".format(
+                    handle.name
+                ))
+                stopwatch = Timer()
                 writer.write(val)
                 writer.flush()
+                lap = stopwatch.lap()
+                self._log.info("Finished writing value into buffer file in {}"
+                               " seconds".format(lap / 1000))
 
         # Not assigning to self.value
         else:
@@ -74,11 +89,15 @@ class ContentWrapper(object):
 
     def __getstate__(self):
         """Being Pickled"""
+        self._log.debug("Being pickled")
+
+        # Close the buffer file if needed
         if isinstance(self.locationHandle, file):
             self.locationHandle.close()
 
         state = self.__dict__.copy()
         del state['locationHandle']
+        del state['_log']  # Delete the logger instance
 
         # Prevent __del__ from deleting the buffer file
         # Needs to come after we've created the state copy so
@@ -89,14 +108,20 @@ class ContentWrapper(object):
 
     def __setstate__(self, state):
         """Being un-Pickled, need to restore state"""
+
         self.__dict__.update(state)
         self.locationHandle = None
+
+        # Reestablish the logger
+        self._initializeLogging()
+        self._log.debug("Being un-pickled")
 
         if self.locationName is not None:
             self.locationHandle = open(self.locationName, "r+b")
 
     def __del__(self):
         """When used, close any open file handles on object destruction"""
+        self._log.debug("Object being deleted")
         if isinstance(self.locationHandle, file):
             self.locationHandle.close()
 
@@ -117,6 +142,8 @@ class ContentWrapper(object):
         return self.value == other
 
     def __init__(self, val):
+        self._initializeLogging()
+
         self.type = ContentWrapper.TYPES.DIRECT
 
         # Used only if this is stored in a file
@@ -126,6 +153,18 @@ class ContentWrapper(object):
 
         # Store the initial value
         self.value = val
+
+    def _initializeLogging(self):
+        if hasattr(self, '_log'):
+            if self._log is not None:
+                return
+
+        # Logging
+        self._log = logging.getLogger(__name__)
+        self.addLoggingHandler(logging.NullHandler())
+
+    def addLoggingHandler(self, handler):
+        self._log.addHandler(handler)
 
     def _createBuffer(self):
         pass
