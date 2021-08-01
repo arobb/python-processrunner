be67bc052d879e1cea42d375c5593305a8ea6175
Andy Robb <andy@andyrobb.com>
2021-08-01 15:56:21 -0700

    Support move to src-based structure

--- make-dist.sh
+++ make-dist.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 # Use rst2html-2.7.py README.rst /dev/null to validate the formatting of README
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-VERSION_FILE="$DIR/VERSION"
+VERSION_FILE="$DIR/src/processrunner/VERSION"
 
 # Working directory must be clean for this to create a distribution file cleanly
-VERSION=$( python -c "import sys; from gitversion import get_git_version; sys.stdout.write(get_git_version())" )
-echo "$VERSION" > $VERSION_FILE
+python $DIR/resources/gitversion/gitversion.py --outfile "$VERSION_FILE"
 
-rm -rf $DIR/processrunner.egg-info
+rm -rf $DIR/src/processrunner.egg-info
 python setup.py sdist
 
 # Remove the VERSION file now that it's in the dist directory
