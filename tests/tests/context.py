1d4f1699132142fb89e458e1f668aaf24317f208
Andy Robb <andy@andyrobb.com>
2021-08-01 15:52:27 -0700

    Move to a src-based structure

--- tests/tests/context.py
+++ tests/tests/context.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 # Provide test modules an easy way to import project stuff
 
-import logging
 import logging.config
 import os
 import sys
 
 # Configure the package search path to include the main package
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 
 # Configure log detail
 log_config_fname = os.path.dirname(__file__) + "/../content/testing_logging_config.ini"
